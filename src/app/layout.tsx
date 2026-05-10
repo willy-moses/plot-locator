91095119a1ed4d41c7e1d8c272e40ed805577bb7
Willy Moses <willkapenamowa@gmail.com>
2026-05-10 13:59:42 +0200

    add PWA support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=DM+Mono:wght@400;500&family=Outfit:wght@300;400;500;600&display=swap" rel="stylesheet" />
+
+        {/* PWA */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1D9E75" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="PlotLocator" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
       </head>
       <body>
         {children}
